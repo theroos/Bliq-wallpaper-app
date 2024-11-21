88203e93cab26e1c9e4b18db55a2e8544becebd7
theroos <roosonline7@gmail.com>
2024-11-21 15:56:28 +0530

    Bliq wallpaper app, day 1. The splashscreen done. Just some customisation left.

--- app/src/main/java/com/example/bliq/SplashScreen.kt
+++ app/src/main/java/com/example/bliq/SplashScreen.kt
@@ -1,26 +1,52 @@
 package com.example.bliq
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashScreen : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
 
+        if (isInternetAvailable(this)) {
+            // Internet is available
             Handler(Looper.getMainLooper()).postDelayed({
-                val intent = Intent(this, MainActivity::class.java)
+                val intent = Intent(this, HomeActivity::class.java)
                 startActivity(intent)
                 finish()
             }, 3000)
+        } else {
+            // No internet
+            Handler(Looper.getMainLooper()).postDelayed({
+            }, 6000)
+            Toast.makeText(this,"Connect the internet",Toast.LENGTH_LONG).show()
         }
+
+        }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isInternetAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
     }
+}
 
 
