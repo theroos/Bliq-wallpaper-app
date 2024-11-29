565623ef8ddf8818a94403e72b9c9657eb6ef460
theroos <roosonline7@gmail.com>
2024-11-30 01:25:53 +0530

    Bliq wallpaper app, day 3. Lots of work done. home screen almost done. two fragments created. kotlin codes started.

--- app/src/main/java/com/example/bliq/HomeActivity.kt
+++ app/src/main/java/com/example/bliq/HomeActivity.kt
@@ -1,20 +1,32 @@
 package com.example.bliq
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bliq.databinding.ActivityMainBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //enableEdgeToEdge()
+        setContentView(binding.root)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        }*/
+
+
+
+
+
     }
 }
